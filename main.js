595a81bd0d99d92c26b7a0faf6d14243f18f2fb3
Mohamed Aziz Hlel <med.aziz.hlel@gmail.com>
2023-09-13 23:06:21 +0100

    2.0
    - Overhaul changes

--- main.js
+++ main.js
@@ -1,9 +1,60 @@
-var form = document.getElementById("contact");
+
+// follow sections
+  $(".main-menu li:first").addClass("active");
+
+  let showSection = function showSection(section, isAnimate) {
+  let direction = section.replace(/#/, ""),
+  reqSection = $(".section").filter(
+  '[data-section="' + direction + '"]'
+  ),
+  reqSectionPos = reqSection.offset().top - 0;
+
+  if (isAnimate) {
+  $("body, html").animate(
+{
+  scrollTop: reqSectionPos,
+},
+  800
+  );
+} else {
+  $("body, html").scrollTop(reqSectionPos);
+}
+};
+
+  let checkSection = function checkSection() {
+    $(".section").each(function () {
+      let $this = $(this),
+          topEdge = $this.offset().top - 300,
+          bottomEdge = topEdge + $this.height(),
+          wScroll = $(window).scrollTop();
+      if (topEdge < wScroll && bottomEdge > wScroll) {
+        let currentId = $this.data("section"),
+            reqLink = $("a").filter("[href*=\\#" + currentId + "]");
+        reqLink
+            .closest("li")
+            .addClass("active")
+            .siblings()
+            .removeClass("active");
+      }
+    });
+  };
+
+  $(".main-menu").on("click", "a", function (e) {
+  e.preventDefault();
+  showSection($(this).attr("href"), true);
+});
+
+  $(window).scroll(function () {
+  checkSection();
+});
+
+// contact form
+
+let form = document.getElementById("contact");
 
 async function handleSubmit(event) {
   event.preventDefault();
-  var status = document.getElementById("my-form-status");
-  var data = new FormData(event.target);
+  let data = new FormData(event.target);
   fetch(event.target.action, {
     method: form.method,
     body: data,
@@ -12,11 +63,12 @@ async function handleSubmit(event) {
     },
   })
     .then((response) => {
-      status.innerHTML = "Thanks for your submission!";
+      alert("E-mail send!!");
       form.reset();
     })
     .catch((error) => {
-      status.innerHTML = "Oops! There was a problem submitting your form";
+      alert("There seems to be a problem!!");
+      console.log(error)
     });
 }
 form.addEventListener("submit", handleSubmit);
